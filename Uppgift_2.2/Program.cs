135b1d70dd88629106d032f7784215be98b0acfb
ercr07001 <ercr07001@E-PW05CGR0>
2024-10-09 12:50:02 +0200

    Add project files.

---
+++ Uppgift_2.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Uppgift_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt har Alma och Elin hoppat? (i meter)");
+            Console.WriteLine("Alma:");
+            string Alma = Console.ReadLine();
+            Console.WriteLine("Alma har hoppat så har långt" + " " + Alma);
+            Console.WriteLine("Elin");
+            string Elin = Console.ReadLine();
+            Console.WriteLine("Elin har hoppat så här långt" + " " + Elin);
+
+            Double Alma1 = Double.Parse(Alma);
+            Double Elin1 = Double.Parse(Elin);
+            Elin1 = Elin1 - Alma1;
+
+            Console.WriteLine("Elin har hoppat" + Elin1 + " " + "mer än Alma");
+
+            Console.ReadKey();
+        }
+    }
+}
